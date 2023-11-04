984e64cce4e689ee56c8676d01fcdd0241856a07
Rohan <rohankumar5803@gmail.com>
2023-11-04 16:37:07 +0530

    initial commit

---
+++ Converter/Converter.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//length
+double cmToMeter(){
+
+}
+
+double meterToCm(){
+
+}
+
+double cmToInch(){
+
+}
+
+double InchToCm(){
+
+}
+
+double cmToMiles(){
+
+}
+
+double milesToCm(){
+
+}
+
+double cmToFeet(){
+
+}
+
+double feetToCm(){
+
+}
+
+double meterToInch(){
+
+}
+
+double InchToMeter(){
+
+}
+
+double meterToMiles(){
+
+}
+
+double milesToMeter(){
+
+}
+
+double meterToFeet(){
+
+}
+
+double feetToMeter(){
+    
+}
+
+int main(){
+
+}
